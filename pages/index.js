21fc8994e86dab62d226e9c09911ccd4aa63d24c
Janis.Christodulow <janis.christodulow@biomes.world>
2021-03-10 23:45:12 +0100

    adds tailwind

--- pages/index.js
+++ pages/index.js
@@ -21,12 +21,9 @@ export default function Home({ allPostsData }) {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>Hi, I'm Janis. I'm a fullstack developer at Biomes.</p>
                 <p>
-                    (This is a sample website - you’ll be building a site like
-                    this on{' '}
-                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>
-                    .)
+                    Hi, I'm Janis. I live in Berlin Germany and currently work
+                    as a fullstack developer at Biomes utilizing the LAMP stack.
                 </p>
             </section>
             <section
---
+++ pages/oil/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+export default function Post() {
+    return (
+        <Layout>
+            <Head>
+                <title>Oil</title>
+            </Head>
+            <h1>Damn</h1>
+        </Layout>
+    );
+}
